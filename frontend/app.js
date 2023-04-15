6e7d9aff1794dd7d39adf1d91413dbbb6bf2493d
rwitzel <rodrigo.witzel@gmx.de>
2023-04-15 16:39:05 +0200

    started

--- frontend/app.js
+++ frontend/app.js
@@ -1,21 +1,8 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.row = [
-    { node: { x: 0, y : 0, name: 'test' } },
-    { node: { x: 50, y : 50, name: 'foo' } }
-    ];
-  $scope.move = function(){
-    angular.forEach($scope.row, function(value, index){
-      value.node.x += Math.round((Math.random()-0.5)*20);
-    });
-  }
   $scope.black = "stroke:rgb(0, 0,0);stroke-width:2";
   $scope.points = {
     "stange_x": 50
   };
-});
-
-app.directive('', function(){
-
 });
