39134f345b9d22ebf34c8376268493cfed5842bb
rwitzel <rodrigo.witzel@gmx.de>
2023-04-15 18:43:59 +0200

    started

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,8 @@ var app = angular.module('plunker', []);
 app.controller('MainCtrl', function($scope) {
     $scope.black = "stroke:rgb(0, 0,0);stroke-width:2";
     $scope.blue = "stroke:rgb(0, 0,255);stroke-width:10";
+    $scope.green = "stroke:green;stroke-width:4";
+    $scope.orange = "stroke:orange;stroke-width:4";
     $scope.points = {
         "stange_x": 50,
         "stange_y": 125,
