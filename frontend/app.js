6946a4a401af8813d16cfaf597a3e0e46fea6d9b
rwitzel <rodrigo.witzel@gmx.de>
2023-04-15 17:04:43 +0200

    started

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,33 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.black = "stroke:rgb(0, 0,0);stroke-width:2";
-  $scope.points = {
-    "stange_x": 50
-  };
+    $scope.black = "stroke:rgb(0, 0,0);stroke-width:2";
+    $scope.points = {
+        "stange_x": 50,
+        "stange_y": 125,
+        "winkel": 45
+    };
+    $scope.points2 = {
+        "aussen_x": 50,
+        "aussen_y": 125
+    };
+
+    function getCircleY(radians, radius) {
+      return Math.sin(radians) * radius;
+    }
+    function getCircleX(radians, radius) {
+      return Math.cos(radians) * radius;
+    }
+
+
+    function update_points2() {
+        $scope.points2 = {
+            "aussen_x": $scope.points.stange_x + getCircleX($scope.points.winkel / 180 * Math.PI, $scope.points.stange_y),
+            "aussen_y": $scope.points.stange_y - getCircleY($scope.points.winkel / 180 * Math.PI, $scope.points.stange_y)
+        };
+        console.log('hey, my Var has changed!');
+    }
+
+    $scope.$watch('points', update_points2, true);
+    update_points2();
 });
